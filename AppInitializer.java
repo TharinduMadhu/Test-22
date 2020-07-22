4aa7cbdd11d50230cc7345f612b8b808e32c3022
Dilini Nadeesha <dilininadeeshawellage@gmail.com>
2020-07-22 13:04:42 +0530

    Initial Commit

---
+++ AppInitializer.java
@@ -0,0 +1,12 @@
+public class AppInitializer {
+
+    public static void main(String[] args) {
+
+        Student S1 = new Student(1, "Dillini");
+        System.out.println(S1);
+
+        Student S2 = new Student(2, "Tharindu");
+        System.out.println(S2);
+        
+    }
+}
